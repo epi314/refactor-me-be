d2d4c7dc2bf2599f053687aa4010433dc167eab9
Pierre Enriquez <pierre@perxtech.com>
2019-05-15 02:30:39 +0800

    Add console app project for demo purposes.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RefactorMe;
+
+namespace ConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("NZD ===============================================");
+            foreach (var product in ProductDataConsolidator.Get())
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Type);
+                Console.WriteLine(product.Price);
+            }
+            Console.WriteLine("===============================================");
+            Console.WriteLine("USD ===============================================");
+            foreach (var product in ProductDataConsolidator.GetInUSDollars())
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Type);
+                Console.WriteLine(product.Price);
+            }
+            Console.WriteLine("===============================================");
+            Console.WriteLine("EUR ===============================================");
+            foreach (var product in ProductDataConsolidator.GetInEuros())
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Type);
+                Console.WriteLine(product.Price);
+            }
+            Console.WriteLine("===============================================");
+        }
+    }
+}
