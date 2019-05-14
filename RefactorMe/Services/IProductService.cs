060b4895a09311969313753e9ac5db9ced3a2635
Pierre Enriquez <pierre@perxtech.com>
2019-05-15 02:22:45 +0800

    Add repository service which consilidates all readonly repositories.

---
+++ RefactorMe/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+using RefactorMe.DontRefactor.Models;
+
+namespace RefactorMe.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts(double rate = 1);
+    }
+}
---
+++ RefactorMe/Services/Implementations/RepositoryService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using RefactorMe.DontRefactor.Models;
+
+namespace RefactorMe.Services.Implementations
+{
+    public class RepositoryService : IProductService
+    {
+
+        private readonly IProductDatasource[] _datasources = {
+            new LawnmowerDatasource(),
+            new PhoneCaseDatasource(),
+            new TShirtDatasource()
+        };
+
+        public RepositoryService()
+        {
+        }
+
+        public List<Product> GetProducts(double rate = 1)
+        {
+            var products = new List<Product>();
+
+            foreach (var source in _datasources)
+            {
+                products.AddRange(source.GetProducts(rate));
+            }
+
+            return products;
+        }
+    }
+}
