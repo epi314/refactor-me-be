20b971c6241e180afbf188346d090e1328f687ea
Pierre Enriquez <pierre@perxtech.com>
2019-05-15 02:29:16 +0800

    Modify to use the service.
    This will serve as a helper class.

--- RefactorMe/ProductDataConsolidator.cs
+++ RefactorMe/ProductDataConsolidator.cs
@@ -1,124 +1,27 @@
-﻿using RefactorMe.DontRefactor.Data.Implementation;
-using RefactorMe.DontRefactor.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using RefactorMe.DontRefactor.Models;
+using RefactorMe.Services;
+using RefactorMe.Services.Implementations;
 
 namespace RefactorMe
 {
     public class ProductDataConsolidator
     {
         public static List<Product> Get() {
-            var l = new LawnmowerRepository().GetAll();
-            var p = new PhoneCaseRepository().GetAll();
-            var t = new TShirtRepository().GetAll();
-
-            var ps = new List<Product>();
-
-            foreach (var i in l) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price,
-                    Type = "Lawnmower"
-                });
-            }
-
-            foreach (var i in p) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price,
-                    Type = "Phone Case"
-                });
-            }
-
-            foreach (var i in t) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price,
-                    Type = "T-Shirt"
-                });
-            }
-
-            return ps;
+            IProductService repoService = new RepositoryService();
+            return repoService.GetProducts();
         }
 
         public static List<Product> GetInUSDollars() {
-            var l = new LawnmowerRepository().GetAll();
-            var p = new PhoneCaseRepository().GetAll();
-            var t = new TShirtRepository().GetAll();
-
-            var ps = new List<Product>();
-
-            foreach (var i in l) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price * 0.76,
-                    Type = "Lawnmower"
-                });
-            }
-
-            foreach (var i in p) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price * 0.76,
-                    Type = "Phone Case"
-                });
-            }
-
-            foreach (var i in t) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price * 0.76,
-                    Type = "T-Shirt"
-                });
-            }
-
-            return ps;
+            IProductService repoService = new RepositoryService();
+            return repoService.GetProducts(0.76);
         }
 
         public static List<Product> GetInEuros() {
-            var l = new LawnmowerRepository().GetAll();
-            var p = new PhoneCaseRepository().GetAll();
-            var t = new TShirtRepository().GetAll();
-
-            var ps = new List<Product>();
-
-            foreach (var i in l) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price * 0.67,
-                    Type = "Lawnmower"
-                });
-            }
-
-            foreach (var i in p) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price * 0.67,
-                    Type = "Phone Case"
-                });
-            }
-
-            foreach (var i in t) {
-                ps.Add(new Product() {
-                    Id = i.Id,
-                    Name = i.Name,
-                    Price = i.Price * 0.67,
-                    Type = "T-Shirt"
-                });
-            }
-
-            return ps;
+            IProductService repoService = new RepositoryService();
+            return repoService.GetProducts(0.67);
         }
  
  
